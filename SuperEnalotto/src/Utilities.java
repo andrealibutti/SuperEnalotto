a82c00a88537f46956a9f494d65d378def4501b9
ikhimenko <49020902+ikhimenko@users.noreply.github.com>
2019-06-21 10:52:55 +0200

    alalallala

--- SuperEnalotto/src/Utilities.java
+++ SuperEnalotto/src/Utilities.java
@@ -4,7 +4,7 @@ public class Utilities {
 
 	public static void main(String[] args) {
 		
-		public void actionPerformed(ActionEvent v) {
+		public void actionPerformed(ActionEvent lalallala) {
 			if (e.getSource()==b) {
 			System.exit(0);
 			}
