cc3a4bd7c499f13820a8b8d36b1077f3e66917a4
ikhimenko <49020902+ikhimenko@users.noreply.github.com>
2019-06-21 11:37:37 +0200

    cliente

---
+++ SuperEnalotto/src/Cliente.java
@@ -0,0 +1,35 @@
+
+public class Cliente {
+	
+	private String nome;
+	private String cognome;
+	private String email;
+	private String tel;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+
+}
