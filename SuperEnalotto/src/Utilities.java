6791acd323c7e259c617bb22671538e4d0957ccc
ikhimenko <49020902+ikhimenko@users.noreply.github.com>
2019-06-21 11:31:51 +0200

    1

--- SuperEnalotto/src/Utilities.java
+++ SuperEnalotto/src/Utilities.java
@@ -9,38 +9,7 @@ public class Utilities {
 	public static void main(String[] args) extends  {
 		
 <<<<<<< HEAD
-		public void actionPerformed(ActionEvent NONFUNZIONAERAMAIIIII) {
-			if (e.getSource()==b) {
-			System.exit(0);
-			}
-			if(e.getSource()==b2) {
-				tf.setText(" ");
-				
-				
-			}
-			if(e.getSource()==b3) {
-				this.testo=tf.getText();
-				if(cc.setPin(testo)) {
-					g2=new Gestione2();
-					this.dispose();
-					System.out.println("Pin giusto");
-					
-				}else {
-					contatore--;
-					l.setText("hai ancora "+contatore+" tentativi");
-					if(contatore==0) {
-						
-						System.exit(0);
-						
-					}
-					
-				}
-				
-				
-		
-				
-			}
-=======
+		=======
 >>>>>>> branch 'master' of https://github.com/andrealibutti/SuperEnalotto.git
 		
 	}
