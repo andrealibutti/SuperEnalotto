fca9b23c817020afb6798d3052074817474d5a69
ikhimenko <49020902+ikhimenko@users.noreply.github.com>
2019-06-21 10:50:37 +0200

    prova e

--- SuperEnalotto/src/Utilities.java
+++ SuperEnalotto/src/Utilities.java
@@ -1,9 +1,41 @@
 
+
 public class Utilities {
 
 	public static void main(String[] args) {
 		
-		//PRova sa sa prova
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource()==b) {
+			System.exit(0);
+			}
+			if(e.getSource()==b2) {
+				tf.setText(" ");
+				
+				
+			}
+			if(e.getSource()==b3) {
+				this.testo=tf.getText();
+				if(cc.setPin(testo)) {
+					g2=new Gestione2();
+					this.dispose();
+					System.out.println("Pin giusto");
+					
+				}else {
+					contatore--;
+					l.setText("hai ancora "+contatore+" tentativi");
+					if(contatore==0) {
+						
+						System.exit(0);
+						
+					}
+					
+				}
+				
+				
+		
+				
+			}
+		
 	}
 
 }
